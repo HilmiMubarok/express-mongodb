4c7f920bcf4a66c3bc0c678710fbcaa5c6491358
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-03-19 14:51:59 +0700

    add morgan for logging

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+// const path = require('path');
+const log = require('morgan');
 
 app.use(cors);
+app.use(log('dev'));
 
 // use url encoded
 app.use(express.urlencoded({ extended: true }));
@@ -11,11 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // express static path join uploads folder
-app.use('/public', express.static(path.join(__dirname, 'public')));
+// app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
 	res.json({ message: 'Hello World.' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server started on port ' + PORT));
